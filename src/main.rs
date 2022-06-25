81512a9072f2d401bb869557f75feeb52c0da8d8
mark <mark@watana.be>
2022-06-25 13:56:53 +0900

    rename url to url_pattern

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ async fn webdriver(browser:String,port:i32,sequence_file:String) -> WebDriverRes
     println!("{:?}",v);
     println!("Version : {}",sequence_parser::get_version(&v));
     println!("Comment : {}",sequence_parser::get_comment(&v));
-    println!("url:{}",sequence_parser::get_url(&v));
+    println!("url:{}",sequence_parser::get_url_pattern(&v));
     driver.quit().await?;
     Ok(())
 }
